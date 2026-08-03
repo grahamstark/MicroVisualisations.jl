82e448a34ca83d194edc6513286095ea0995c004
Graham Stark <graham.stark@virtual-worlds.biz>
2026-08-03 17:00:38 +0100

    MicroAPI 21/1 better (?) format and label big costs table

--- src/table-generation.jl
+++ src/table-generation.jl
@@ -98,7 +98,18 @@ function format_run_settings_summary( settings :: Settings, format::Union{MV_MAR
 end
 
 function format_detailed_costs( incs1::DataFrame, incs2::DataFrame, format::Union{MV_MARKDOWN,MV_HTML,MV_TYPST} )::String
-    return labelled_frame_to_table( detailed_cost_dataframe( incs1, incs2 ), format )
+    df = detailed_cost_dataframe( incs1, incs2 )
+    df.Count1 ./= 1000
+    df.Count2 ./= 1000
+    df.DCount ./= 1000
+    rename!( df, [
+            :Value1=>"Amounts(Before) £m",
+            :Count1=>"Counts(Before) 000s",
+            :Value2=> "Amounts(After) £m",
+            :Count2=>"Counts(After) 000s",
+            :Dval=>"∇ £M",
+            :Dcount=>"∇ Count"])
+    return labelled_frame_to_table( df, format; prec=0 )
 end
 
 #=
